106f03e9706b1428b7a7d4826d132721556f85b3
cyberneticocult <cyberneticocult@gmail.com>
2022-08-17 00:36:55 -0400

    Added custom properties and ui panel to object type for collider settings.

--- io_scene_gltf2_omi_collision/__init__.py
+++ io_scene_gltf2_omi_collision/__init__.py
@@ -1,6 +1,6 @@
 import bpy
-from bpy.types import PropertyGroup, Scene, Panel
-from bpy.props import BoolProperty, PointerProperty, FloatProperty
+from bpy.types import PropertyGroup, Scene, Panel, Operator, Object, PropertyGroup
+from bpy.props import BoolProperty, PointerProperty, FloatProperty, EnumProperty
 from bpy.utils import register_class, unregister_class
 
 bl_info = {
@@ -19,6 +19,23 @@ bl_info = {
 glTF_extension_name = 'OMI_collider'
 
 extension_is_required = False
+
+collider_types = [
+    ('box', 'Box', ''),
+    ('sphere', 'Sphere', ''),
+    ('capsule', 'Capsule', ''),
+    ('hull', 'Hull', ''),
+    ('mesh', 'Mesh', ''),
+    ('compound', 'Compound', '')
+]
+
+# collider_properties_name = 'gltf2_omi_collider_properties'
+# collider_properties_defaults = dict(collider_type=collider_types[0], collider_istrigger=0)
+
+class OMIColliderProperties(PropertyGroup):
+    is_collider: BoolProperty(name='Is Collider')
+    collider_type: EnumProperty(items=collider_types, name='Collider Type')
+    collider_is_trigger: BoolProperty(name='Is Trigger')
 
 class OMIColliderExportExtensionProperties(PropertyGroup):
     enabled: BoolProperty(
@@ -108,7 +125,59 @@ class GLTF_PT_OMIColliderImportExtensionPanel(Panel):
         box.label(text=glTF_extension_name)
 
         layout.prop(props, 'float_property', text="test float value")
+
+def _is_mesh_object_active(context):
+    objs = context.selected_objects
+    return True if len(objs) > 0 and context.active_object.type == 'MESH' else False
+    
+class GLTF_PT_OMIColliderObjectPropertiesPanel(Panel):
+
+    bl_label = 'glTF OMI_collider Properties'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'object'
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_order = 1001 # order the addon properties to last
+
+    @classmethod
+    def poll(cls, context):
+        return True if _is_mesh_object_active(context) else None
+
+    def draw(self, context):
+        active_obj = context.active_object
+        collider_props = active_obj.OMIColliderProperties
         
+        layout = self.layout
+
+        layout.prop(collider_props, 'is_collider')
+        
+        if collider_props is not None and collider_props.is_collider:
+            layout.prop(collider_props, 'collider_type')
+            layout.prop(collider_props, 'collider_is_trigger')
+
+# class GLTF_OT_OMIColliderAddProperties(Operator):
+
+#     bl_idname = 'gltf2_omi_collider_extension.add_properties'
+#     bl_label = 'Add OMI_Collider Properties'
+#     bl_description = 'Add the properties describing the collider to the object.'
+#     bl_options = {'REGISTER', 'UNDO'}
+
+#     @classmethod
+#     def poll(cls, context):
+#         return True if _is_mesh_object_active(context) else None
+
+#     def execute(self, context):
+#         active_obj = context.active_object
+#         collider_props = active_obj.get(collider_properties_name, None)
+
+#         if collider_props is None:
+#             active_obj[collider_properties_name] = collider_properties_defaults.copy()
+        
+#         return {'FINISHED'}
+    
+#     def invoke(self, context, event):
+#         return self.execute(context)
+
 class glTF2ExportUserExtension:
 
     def __init__(self):
@@ -117,7 +186,14 @@ class glTF2ExportUserExtension:
         self.properties = bpy.context.scene.OMIColliderExportExtensionProperties
 
     def gather_node_hook(self, gltf2_object, blender_object, export_settings):
-        if self.properties.enabled: pass
+        if self.properties.enabled:
+            if gltf2_object.extensions is None: gltf2_object.extensions = {}
+
+            gltf2_object.extensions[glTF_extension_name] = self.Extension(
+                name=glTF_extension_name,
+                extension={'float': self.properties.float_property},
+                required=extension_is_required
+            )
 
 class glTF2ImportUserExtension:
 
@@ -132,35 +208,39 @@ class glTF2ImportUserExtension:
     def gather_import_node_after_hook(self, vnode, gltf_node, blender_object, import_settings):
         if self.properties.enabled:
             pass
-        
-def register_panel():
-    try: register_class(GLTF_PT_OMIColliderExportExtensionPanel)
-    except Exception: pass
 
-    try: register_class(GLTF_PT_OMIColliderImportExtensionPanel)
-    except Exception: pass
-    
-    return unregister_panel
+addon_classes = [
+    OMIColliderExportExtensionProperties,
+    OMIColliderImportExtensionProperties,
+    OMIColliderProperties,
+    GLTF_OT_OMIColliderAddProperties,
+    GLTF_PT_OMIColliderObjectPropertiesPanel
+]
+
+extension_panel_classes = [
+    GLTF_PT_OMIColliderExportExtensionPanel,
+    GLTF_PT_OMIColliderImportExtensionPanel    
+]
 
 def unregister_panel():
-    try: unregister_class(GLTF_PT_OMIColliderExportExtensionPanel)
-    except Exception: pass
+    for cls in extension_panel_classes: unregister_class(cls)
 
-    try: unregister_class(GLTF_PT_OMIColliderImportExtensionPanel)
-    except Exception: pass    
-
+def register_panel():
+    for cls in extension_panel_classes: register_class(cls)
+    return unregister_panel
+    
 def register():
-    register_class(OMIColliderExportExtensionProperties)
+    for cls in addon_classes: register_class(cls)
+    
     Scene.OMIColliderExportExtensionProperties = PointerProperty(type=OMIColliderExportExtensionProperties)
-
-    register_class(OMIColliderImportExtensionProperties)
     Scene.OMIColliderImportExtensionProperties = PointerProperty(type=OMIColliderImportExtensionProperties)
 
-def unregister():
-    unregister_panel()
-    
-    unregister_class(OMIColliderExportExtensionProperties)
-    del Scene.OMIColliderExportExtensionProperties
+    Object.OMIColliderProperties = PointerProperty(type=OMIColliderProperties)
 
-    unregister_class(OMIColliderImportExtensionProperties)
+def unregister():
+    for cls in reversed(addon_classes): unregister_class(cls)
+
+    del Scene.OMIColliderExportExtensionProperties
     del Scene.OMIColliderImportExtensionProperties
+
+    del Object.OMIColliderProperties
